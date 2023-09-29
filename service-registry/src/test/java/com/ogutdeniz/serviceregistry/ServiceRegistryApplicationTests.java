3daabc9d6c54ad8a3c45798e1193f65c20ba1761
Deniz Ogut <denizogut123@gmail.com>
2023-09-29 19:25:51 +0300

    UPDATE
    Service-Registration with Eureka Server added

---
+++ service-registry/src/test/java/com/ogutdeniz/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogutdeniz.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- weatherservice/src/main/java/com/ogutdeniz/weatherservice/WeatherServiceApplication.java
+++ weatherservice/src/main/java/com/ogutdeniz/weatherservice/WeatherServiceApplication.java
@@ -2,7 +2,7 @@ package com.ogutdeniz.weatherservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
+
 
 @SpringBootApplication
 public class WeatherServiceApplication {
