e70b122a50caa182bf2ab650e846ab37daff0053
Deniz Ogut <denizogut123@gmail.com>
2023-10-01 16:09:44 +0300

    UPDATE
    Location Service Skeleton Created

---
+++ locationservice/src/main/java/com/ogutdeniz/locationservice/LocationserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ogutdeniz.locationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationserviceApplication.class, args);
+	}
+
+}
---
+++ locationservice/src/test/java/com/ogutdeniz/locationservice/LocationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogutdeniz.locationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
