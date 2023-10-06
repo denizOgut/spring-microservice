94c64edbb4d94ed233cf724d130c170d9a014b68
Deniz Ogut <denizogut123@gmail.com>
2023-10-06 19:58:03 +0300

    UPDATE
    Swagger added for locationservice

---
+++ locationservice/src/main/java/com/ogutdeniz/locationservice/config/OpenApiConfig.java
@@ -0,0 +1,34 @@
+package com.ogutdeniz.locationservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Location Service API")
+                        .version("1.0")
+                        .description("""
+                                This is an api provides location info 
+                                based on city,country,continent names and long - lat
+                                """
+                        )
+                        .contact(new Contact()
+                                .name("Deniz Öğüt")
+                                .email("denizogut123@gmail.com")
+                                .url("https://www.linkedin.com/in/deniz-öğüt/")
+                        )
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")
+                        )
+                );
+    }
+}
--- locationservice/src/main/java/com/ogutdeniz/locationservice/controller/LocationController.java
+++ locationservice/src/main/java/com/ogutdeniz/locationservice/controller/LocationController.java
@@ -4,7 +4,14 @@ import com.ogutdeniz.locationservice.dto.APIResponseDto;
 import com.ogutdeniz.locationservice.dto.LocationDto;
 import com.ogutdeniz.locationservice.model.Location;
 import com.ogutdeniz.locationservice.service.LocationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/location-service")
+@Tag(name = "Location Service API v1", description = "Location Service API to find location information's based on geographical data")
 public class LocationController {
 
     private final LocationService locationService;
@@ -20,26 +28,156 @@ public class LocationController {
         this.locationService = locationService;
     }
 
+    @Operation(
+            method = "GET",
+            summary = "find the location information's based on city.",
+            description = "find the location information's based on city.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "The location information's of the city",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    array = @ArraySchema(schema = @Schema(implementation = APIResponseDto.class))
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "City name is wrong. Re-try with a valid city name",
+                            content = @Content(schema = @Schema(hidden = true))
+                    ),
+
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal/External server error.",
+                            content = @Content(schema = @Schema(hidden = true))
+                    )
+            }
+    )
     @GetMapping("/{city}")
     public ResponseEntity<APIResponseDto> findLocationByCity(@PathVariable String city) {
         return ResponseEntity.ok(locationService.findLocationByCity(city));
     }
 
+    @Operation(
+            method = "GET",
+            summary = "find the location information's based on longitude and latitude.",
+            description = "find the location information's based on longitude and latitude.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "The location information's of the longitude and latitude",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    array = @ArraySchema(schema = @Schema(implementation = APIResponseDto.class))
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Longitude and latitude are wrong. Re-try with a valid longitude and latitude",
+                            content = @Content(schema = @Schema(hidden = true))
+                    ),
+
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal/External server error.",
+                            content = @Content(schema = @Schema(hidden = true))
+                    )
+            }
+    )
     @GetMapping("/{longitude}/{latitude}")
     public ResponseEntity<APIResponseDto> findLocationByLongitudeAndLatitude(@PathVariable Double longitude, @PathVariable Double latitude) {
         return ResponseEntity.ok(locationService.findLocationByLongitudeAndLatitude(longitude, latitude));
     }
 
+    @Operation(
+            method = "GET",
+            summary = "find the location information's based on country.",
+            description = "find the location information's based on country.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "The location information's of the country",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    array = @ArraySchema(schema = @Schema(implementation = APIResponseDto.class))
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "City name is wrong. Re-try with a valid country name",
+                            content = @Content(schema = @Schema(hidden = true))
+                    ),
+
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal/External server error.",
+                            content = @Content(schema = @Schema(hidden = true))
+                    )
+            }
+    )
     @GetMapping("{country}")
     public ResponseEntity<List<APIResponseDto>> findAllLocationsByCountry(@PathVariable String country) {
         return ResponseEntity.ok(locationService.findAllLocationsByCountry(country));
     }
 
+    @Operation(
+            method = "GET",
+            summary = "find the location information's based on continent.",
+            description = "find the location information's based on continent.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "The location information's of the continent",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    array = @ArraySchema(schema = @Schema(implementation = APIResponseDto.class))
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "City name is wrong. Re-try with a valid continent name",
+                            content = @Content(schema = @Schema(hidden = true))
+                    ),
+
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal/External server error.",
+                            content = @Content(schema = @Schema(hidden = true))
+                    )
+            }
+    )
     @GetMapping("{continent}")
     public ResponseEntity<List<APIResponseDto>> findAllLocationsByContinent(@PathVariable String continent) {
         return ResponseEntity.ok(locationService.findAllLocationsByCountry(continent));
     }
 
+    @Operation(
+            method = "POST",
+            summary = "save the location into db.",
+            description = "save the location into db.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "The location saved into db",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    array = @ArraySchema(schema = @Schema(implementation = APIResponseDto.class))
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "No location record saved",
+                            content = @Content(schema = @Schema(hidden = true))
+                    ),
+
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal/External server error.",
+                            content = @Content(schema = @Schema(hidden = true))
+                    )
+            }
+    )
     @PostMapping("/locations")
     public ResponseEntity<LocationDto> saveLocation(@Valid @RequestBody Location location) {
         return ResponseEntity.ok(locationService.saveLocation(location));
