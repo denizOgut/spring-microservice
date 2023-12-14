bdfe282feedd76486ee881c2e4ba4fe103233027
Deniz Ogut <denizogut123@gmail.com>
2023-12-14 19:19:13 +0300

    Rate Limiter Added

--- booking-service/src/main/java/com/ogutdeniz/bookingservice/service/BookingService.java
+++ booking-service/src/main/java/com/ogutdeniz/bookingservice/service/BookingService.java
@@ -5,6 +5,7 @@ import com.ogutdeniz.bookingservice.dto.BookingApiResponseDto;
 import com.ogutdeniz.bookingservice.dto.BookingDTO;
 import com.ogutdeniz.bookingservice.exception.BookingCodeNotFound;
 import com.ogutdeniz.bookingservice.repository.BookingRepository;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -20,7 +21,7 @@ public class BookingService {
         this.bookingRepository = bookingRepository;
         this.flightClient = flightClient;
     }
-
+    @RateLimiter(name = "bookingCode", fallbackMethod = "getDefaultBookingApiResponseDtoFallbackMethod")
     public BookingApiResponseDto findByBookingCode(UUID bookingCode) {
         var booking = bookingRepository.findByBookingCode(bookingCode)
                                        .orElseThrow(() -> new BookingCodeNotFound("Booking not found for given booking code" + bookingCode));
@@ -28,6 +29,11 @@ public class BookingService {
         var bookingDto = BookingDTO.convertToBookingDto(booking);
 
         return new BookingApiResponseDto(bookingDto,flight);
+    }
+
+    private BookingApiResponseDto getDefaultBookingApiResponseDtoFallbackMethod() {
+        logger.error("Booking not found for given booking code");
+        return new BookingApiResponseDto(null, null);
     }
 
 }
